0bb9981232da0ac7c2c08b49be310771bb0c7f31
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-01-24 22:51:45 -0300

    refactor: cors server added

--- server/src/main/java/com/rafaelfachinelli/users/controller/UserController.java
+++ server/src/main/java/com/rafaelfachinelli/users/controller/UserController.java
@@ -8,6 +8,7 @@ import com.rafaelfachinelli.users.repository.UserRepository;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping({ "/api/users" })
 public class UserController {
 
